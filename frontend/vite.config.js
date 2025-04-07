cb71af9a935329ce69f3d7ce56ecb2e5a67b34d6
Christian Landgren <christian@landgren.nu>
2025-04-07 14:44:09 +0200

    fix: run vite scaffold

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/oauth2': {
-        target: 'http://oauth2-proxy:4180',
-        changeOrigin: true,
-      },
-      '/api': {
-        target: 'http://api:3001',
-        changeOrigin: true,
-      }
-    }
-  }
 })
